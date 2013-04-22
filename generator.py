441370f0e0229762b0df1a0e9c6c1bb68632d2e7
brennanrieger <brennanrieger@college.harvard.edu>
2013-04-21 23:37:36 -0400

    includes generator class (without generate function)

---
+++ generator.py
@@ -0,0 +1,46 @@
+#Constants
+num_mazes = 10
+
+
+class Generator:
+	#FIELDS
+	start_loc_x = 1
+	start_loc_y = 1
+	p_jump = 1
+	p_forward = 1
+	p_birds_eye = 1
+	return_dist = 1
+	end_time = 1
+	mazes = []
+	avg_runtime = False
+	parameter_list = [start_loc_x,start_loc_y,p_jump,p_forward,p_birds_eye,return_dist,end_time,mazes,avg_runtime]
+	
+	#METHODS
+	
+	#Constructor takes in params, float list of [start_loc_x,start_loc_y,p_jump,p_forward,p_birds_eye,
+	#return_dist,end_time]
+	def __init__(self,params):
+		start_loc_x = params[0]
+		start_loc_y = params[1]
+		p_jump = params[2]
+		p_forward = params[3]
+		p_birds_eye = params[4]
+		return_dist = params[5]
+		end_time = params[6]
+		
+	
+	def calc_avg_runtime(self):
+		total_time = 0
+		for maze in mazes:
+			total_time = total_time + maze.runtime
+		return total_time/len(mazes)
+	
+
+
+
+
+
+
+
+
+	
