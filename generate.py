bf115e137e790ecaf56e7e0f161a68448bfc4006
Akua Abu <aabu@college.harvard.edu>
2013-04-22 23:13:09 -0400

    gen working on

---
+++ generate.py
@@ -0,0 +1,50 @@
+import random, sys
+
+class generator:
+# Function definition is here
+    def generate(self):
+
+        # variables that I get from elsewhere
+        x_dim = 20
+        y_dim = 30
+        start_loc_x = 0.23
+        start__y = 0.87
+        p_birds_eye = 0.76
+        # if jumping randomly, then list of usable squares. if p_birds eye succeeds, do birds_eye implement
+
+        return_dist = 0.87
+        p_jump = 0.789
+
+        # lsit of tuples. 1st 
+        usable_squares = []
+        
+    
+        # variables from my code
+        should_jump = random.uniform(0,1)
+        should_birds = random.unifrom(0,1)
+        direction_rand = random.randrange (0, 3, 1)
+
+        North = 0
+        South = 1
+        West  = 2
+        East  = 3
+    
+        coordinates = (start_loc_x, start_loc_y)
+        def directio =
+            return_dis * 
+
+        # ifi you can go there, true
+        # if you can't false
+    
+        end_time
+        def jump :
+            if should_birds < p_birds_eye:
+                return_dist 
+
+        # outside of nested function- just run main   
+        if should_jump < p_jump:
+            for i = 1:len(usable_squares)
+        
+    if
+    
+    return;
