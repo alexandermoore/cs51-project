ce939e29a95a52f3c3d88c643aa278dc02f9a579
Alexander Moore <alexandermoore@college.harvard.edu>
2013-04-28 23:48:57 -0400

    Updated generation.py to terminate after a certain number of attempts at a better generation.

--- main.py
+++ main.py
@@ -3,40 +3,54 @@ from generation import *
 
 # Create a new generation
 # 5 fittest breed next generation, population size of 250, 50 of which are random and 2 of which are the fittest from the previous generation.
-current_gen = Generation(5,5,1,1)
+current_gen = Generation(5,5,2,1)
 current_gen.spawn_random_generation()
 
 # Set the definition of "no more progress"-- evolution
 # after this)
 negligible = 0.00001
 
-# Make sure new_fitness - old_fitness cannot be > negligible 
+# Make sure new_fitness - best_fitness cannot be < negligible 
 # on first run
-old_fitness = -negligible - 1
+best_fitness = -negligible - 1
 
-#Actual loop
+# Actual loop
 gen_num = 0
+bad_gen_count = 0
+# Terminate after 10 successive negligible generations.
+bad_gen_termination = 10
+best_generator = None
+
 while(True) :
     gen_num = gen_num + 1
     print "\nGENERATION #"+str(gen_num)
     # run this generation and get its fitness
     new_fitness = current_gen.run()
-    print "BEST:"
-    print current_gen.fittest[0].avg_runtime
-    print "DIFFERENCE FROM LAST GENERATION:"
-    print new_fitness-old_fitness
+    print "BEST FROM THIS GENERATION:"
+    print new_fitness
+    print "CURENT BEST:"
+    print best_fitness
+    print "DIFFERENCE FROM BEST GENERATION:"
+    print new_fitness-best_fitness
     # if change is non-negligible then spawn the next
     # generation and try again. Otherwise you've got an
     # optimal generation.
-    if (new_fitness - old_fitness > negligible) :
-        old_fitness = new_fitness
-        current_gen = current_gen.spawn_next_generation()
+    if (new_fitness - best_fitness > negligible) :
+        bad_gen_count = 0
+        best_generator = current_gen.fittest[0]
+        best_fitness = new_fitness
     else :
-        break
+        bad_gen_count = bad_gen_count + 1
+        print "BAD GENERATION "+str(bad_gen_count)+"\n"
+        if (bad_gen_count == bad_gen_termination) :
+            break
+    # Spawn the next generation
+    current_gen = current_gen.spawn_next_generation()
 
 
 print "\n\nBEST GENERATOR: "
-print current_gen.fittest[0].parameter_list
+print best_generator.parameter_list
+print best_generator.avg_runtime
 
 # Display the fittest maze of them all	
 #display_object = MazeDisplay()
