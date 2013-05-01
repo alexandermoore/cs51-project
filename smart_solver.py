a8199b261e84a7994bcf6a1a4de05b5049ef8a22
Un Sil Park <unpark@college.harvard.edu>
2013-04-30 22:22:32 -0400

    patchy smart solver

--- smart_solver.py
+++ smart_solver.py
@@ -9,15 +9,17 @@ class SmartSolver:
     def smart_solver(self,m):	# maze to m
 
         # initialize current position to start
-        #rc = [maze.start[0],maze.start[1]]
-        rc = [0,0]
+        #rc = [m.start[0],m.start[1]]
+        #rc = [0,0]
+        m.r = 0 #m.start[0]
+        m.c = 0 #m.start[1]
         m.runtime = 0
         usable = []
           
         def solve(m,rc,usable):
 
             end = [m.end[0],m.end[1]]    # when awake, think about whether end should stay here or go outside solve()
-            board = m.board
+            board = m.board              # actually we can get rid of it and just access it by doing m.end
 
             dir_dict = dict()
             dir_dict["N"] = None
@@ -54,49 +56,55 @@ class SmartSolver:
             # expects direction_headed to be one of: "N", "S", "W", "E"
             def get_next_square(rc,direction_headed):
                 if direction_headed == "N":
-                    return rc[0]-1,rc[1]
+                    return m.r-1,m.c #rc[0]-1,rc[1]
                 elif direction_headed == "S":
-                    return rc[0]+1,rc[1]
+                    return m.r+1,m.c #rc[0]+1,rc[1]
                 elif direction_headed == "W":
-                    return rc[0],rc[1]-1
+                    return m.r,m.c-1 #rc[0],rc[1]-1
                 else: # "E"
-                    return rc[0],rc[1]+1
+                    return m.r,m.c+1 #rc[0],rc[1]+1
 
             ''' distance
             Calculates the distance between a square and the end square.
             RETURNS: distance between two squares
             '''
-      	    def distance(rc,end):
-                distance_from_end = sqrt((end[0]-rc[0])**2 + (end[1]-rc[1])**2)
+      	    def distance(m,end):   #rc,end  # might wanna change it to m -to get m.end- and rc for current point
+                distance_from_end = sqrt((end[0]-m.r)**2 + (end[1]-m.c)**2)
+                #distance_from_end = sqrt((end[0]-rc[0])**2 + (end[1]-rc[1])**2)
                 return distance_from_end
 
             ''' in_usable
             Checks whether a square is in the usable list.
-            RETURNS: True or False
+            RETURNS: boolean
+            -rc: current position
+            -usable: <...> ; sorted from least to greatest distance from end
+            -i: a local counter; expects to be passed in a 0 when first called
             '''
-            # i should always be 0 when first passed in
-            # usable should be sorted from smallest to greatest distance from end
-            def in_usable(rc,usable,i):
+            def in_usable(rc,usable,i): #rc is just any coordinate
                 if i < len(usable):
                     if rc == usable[i][0]:
                         return True
                     else:
-                        i += 1
-                        in_usable(rc,usable,i)
+                        in_usable(rc,usable,i+1)
                 else:
                     return False
-            
-            def walkable(rc,board,direction_headed,usable):
+
+            ''' walkable
+            a            
+            '''                        
+            def walkable(m,board,direction_headed,usable):
                 new = get_next_square(rc,direction_headed)
-                print ["new",new]
-                if (new[0] >= 0 and new[0] < maze_num_rows and new[1] >= 0 and new[1] < maze_num_cols):
-                    if(board[new[0]][new[1]] == True and in_usable(new,usable,0) == False):
+                if (m.r >= 0 and m.r < maze_num_rows and m.c >= 0 and m.c < maze_num_cols):
+                    if(board[m.r][m.c] == True and in_usable(new,usable,0) == False):
                         return (True,(new[0],new[1]))
                     else:
                         return (False,(None,None))
                 else:
                     return (False,(None,None))
-            
+
+            ''' move
+            a
+            '''
             def move(m,rc,new,usable):
                 rc[0] = new[0]
                 rc[1] = new[1]
@@ -105,12 +113,15 @@ class SmartSolver:
                     return # AND WE ARE DONE!
                 else:
                     solve(m,rc,usable)
-            
+
+            ''' jump
+            a
+            '''
             def jump(rc,usable,i): # Find the square in usable that's the closest to the end
                 if usable[i][2] == True:
                     usable[i][2] = False
                     new = usable[i][0]
-                    move(m,rc,new,usable) 
+                    move(m,rc,new,usable)  # we want to solve? 
                 else: # usable[i][2] == False:
                     i += 1
                     print i
@@ -118,7 +129,10 @@ class SmartSolver:
                         jump(rc,usable,i)
                     else:
                         print "No available square in usable."
-            
+
+            ''' step
+            a
+            '''
             def step(m,rc,end,board,visit_order,usable,i):
                 if i < 4:
                     if walkable(rc,board,visit_order[i][0],usable)[0] == True:
