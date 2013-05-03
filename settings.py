40bd2f4fa4928f64e9898becfd2dca028cdcc50c
Alexander Moore <alexandermoore@college.harvard.edu>
2013-05-02 22:52:58 -0400

    made some changes to main

--- settings.py
+++ settings.py
@@ -2,9 +2,8 @@
 Note this is a temporary file to allow me to change global settings without having to
 import main (and hence run the whole program).
 '''
-
 display_maze_generation_in_real_time = False
-display_all_outputted_mazes = True
+display_all_outputted_mazes = False
 maze_num_rows = 20
 maze_num_cols = 20
 num_mazes = 10
